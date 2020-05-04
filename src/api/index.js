a49b5515372d38d955f29cda5319e89af9c5334a
Lexa <lexaraea@gmail.com>
2020-05-04 16:49:07 -0400

    Add country picker option

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,20 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url;
+
+    if (country) {
+        changeableUrl = `${url}/countries/${country}`
+    }
+
     try {
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
         const modifiedData = { confirmed, recovered, deaths, lastUpdate }
 
         return  {  confirmed, recovered, deaths, lastUpdate }; 
     } catch (error) {
+        console.log(error);
 
     }
 }
@@ -29,7 +36,7 @@ export const fetchDailyData = async () => {
     }
 }
 
-export const countries = async () => {
+export const fetchCountries = async () => {
     try {
         const { data: { countries }} = await axios.get(`${url}/countries`);
 
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -3,23 +3,22 @@ import { NativeSelect, FormControl } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
 
-const CountryPicker = () => {
+const CountryPicker = ( {handleCountryChange} ) => {
     const [fetchedCountries, setFetchedCountries] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
-            setFetchedCountries (await fetchCountries());
+            setFetchedCountries(await fetchCountries());
         }
 
-        fetchAPI();
+        fetchAPI(); 
     }, [setFetchedCountries]);
-
-    console.log(fetchedCountries);
 
     return (
         <FormControl className={styles.formControl}>
-            <NativeSelect>
+            <NativeSelect defaultValue="" onChange>
                 <option value="global">Global</option>
+                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
     )
